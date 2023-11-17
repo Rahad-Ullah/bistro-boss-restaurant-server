6cc97ad432fc0f5ba9cda8b1a88fa78413ecd72f
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-11-17 12:39:55 +0600

    Server created successfully

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+    res.send('Boss Server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Restaurant server is running on port ${port}`)
+})
