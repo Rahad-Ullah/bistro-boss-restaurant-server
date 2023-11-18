c87be67d78d0ece0defb1e0a7085102e7d4a5b3e
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-11-18 10:16:51 +0600

    getting all menus and reviews has been implemented successfully

--- index.js
+++ index.js
@@ -27,14 +27,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const database = client.db('bistroBossDB');
+    const menuCollection = database.collection('menu');
+    const reviewCollection = database.collection('reviews');
+
+    // get all menu
+    app.get('/menu', async (req, res) => {
+        const result = await menuCollection.find().toArray()
+        res.send(result)
+    })
+
     
+    // get all reviews
+    app.get('/reviews', async (req, res) => {
+        const result = await reviewCollection.find().toArray()
+        res.send(result)
+    })
+
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
