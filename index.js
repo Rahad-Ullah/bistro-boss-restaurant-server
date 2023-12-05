816f3dd818251df5b13b91c9238319d449f3cbc7
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-12-05 12:30:39 +0600

    Automatic Email System has been added successfully

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const cors = require('cors');
 var jwt = require('jsonwebtoken');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({
+	username: 'api',
+	key: process.env.MAIL_GUN_API_KEY,
+});
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -209,7 +216,26 @@ async function run() {
         $in: payment.cartIds.map(id => new ObjectId(id))
       }}
 
+      // clear cart after payment
       const deleteResult = await cartCollection.deleteMany(query)
+
+      // send email after payment
+      mg.messages
+      .create(process.env.MAIL_SENDING_DOMAIN, {
+        from: "Mailgun Sandbox <postmaster@sandboxc2fddb9056fd4e96baaf433c0ee97a3b.mailgun.org>",
+        to: ["myemail.abc20@gmail.com"],
+        subject: "Bistro Boss Confirmation",
+        text: "Testing some Mailgun awesomness!",
+        html: `
+        <div>
+          <h2>Thank you for your order</h2>
+          <h4>Your Trans. Id: <strong>${payment.transactionId}</strong></h4>
+          <p>We would like to get you feedback about food</p>
+        </div>
+        `
+      })
+      .then(msg => console.log(msg)) // logs response data
+      .catch(err => console.log(err)); // logs any error`;
 
       res.send({result, deleteResult})
     })
