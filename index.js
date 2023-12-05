310ecf8d53580bb8c555bef0778b9cb4c0f9560a
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-12-05 10:55:54 +0600

    Stripe Payment system has been implemeted

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -32,10 +34,65 @@ async function run() {
     const menuCollection = database.collection('menu');
     const reviewCollection = database.collection('reviews');
     const cartCollection = database.collection('carts');
+    const paymentCollection = database.collection('payments');
 
+    //! generate jwt token
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token})
+    })
+
+    // verify token middle ware
+    const verifyToken = (req, res, next) =>{
+      // send error if token doesn't exist
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'fobidden access'})
+      }
+      // verify token if exist
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'fobidden access'})
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
+
+
+    // verify admin middleware
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next()
+    }
+
+
+    // check if the user an admin or not
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email) {
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin'
+      }
+      res.send({admin})
+    }) 
+
+    
     //! users related api
     // get all users
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result)
     })
@@ -52,8 +109,21 @@ async function run() {
       res.send(result)
     })
 
+    // make admin
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
     // delete user
-    app.delete('/users/:id', async (req, res) => {
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await userCollection.deleteOne(query)
@@ -64,6 +134,14 @@ async function run() {
     app.get('/menu', async (req, res) => {
         const result = await menuCollection.find().toArray()
         res.send(result)
+    })
+
+    
+    // insert item into menu
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
     })
 
     
@@ -93,6 +171,47 @@ async function run() {
       const query = {_id: new ObjectId(id)}
       const result = await cartCollection.deleteOne(query)
       res.send(result)
+    })
+
+
+    // payment intent api
+    app.post('/create-payment-intent', async (req, res) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100)
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      })
+    })
+
+
+    // get all payments of specific user
+    app.get('/payments/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = {email: email}
+      const result = await paymentCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // save payments on database
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const result = await paymentCollection.insertOne(payment)
+
+      // carefully delete each item from cart
+      const query = {_id: {
+        $in: payment.cartIds.map(id => new ObjectId(id))
+      }}
+
+      const deleteResult = await cartCollection.deleteMany(query)
+
+      res.send({result, deleteResult})
     })
     
 
